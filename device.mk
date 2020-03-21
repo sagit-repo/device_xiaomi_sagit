1d9701182b2747f49cf95e043ada0a3e67bce48f
dan38 <danilo3840@gmail.com>
2020-03-21 00:04:17 -0300

    Ship LibsAlexey60fps

--- device.mk
+++ device.mk
@@ -57,3 +57,5 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/xiaomi/sagit/sagit-vendor.mk)
 $(call inherit-product-if-exists, vendor/apps/OPScreenRecord/config.mk)
 $(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
+$(call inherit-product-if-exists, vendor/LibsAlexey60fps/config.mk)
+
